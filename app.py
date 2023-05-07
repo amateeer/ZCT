0fef850af788507a6debc55209e19ec1864e6486
amateeer <132763810+amateeer@users.noreply.github.com>
2023-05-07 16:49:22 +0200

    update

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from flask import Flask, request
-from google.cloud import firestore
-from google.auth import credentials
+from pymongo import MongoClient
 
-# Načítanie poverení zo súboru s kľúčom pre službu
-cred = credentials.Certificate('zct1-385912-c6a1e78d96c4.json')
-db = firestore.Client(credentials=cred, project='zct1-385912')
-#
+# Pripojenie k MongoDB Atlas
+connection_string = 'mongodb+srv://zctzadanie:zadanie@zadanie.twkwmzm.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(connection_string)
+
+# Výber databázy a kolekcie
+db = client['zadanie']
+tasks_collection = db['task']
+
 app = Flask(__name__)
 
-db = firestore.Client()
-
-# pridajte zdroj do db
-# pridajte úlohu do databázy
+# Pridajte úlohu do databázy
 def add_task_to_db(task_name, task_desc):
-    tasks_ref = db.collection('tasks')
-    tasks_ref.document().set({
+    task = {
         'task_name': task_name,
         'task_desc': task_desc,
-    })
-
+    }
+    tasks_collection.insert_one(task)
 
 @app.route('/tasks', methods=['POST'])
 def add_task():
@@ -26,7 +25,6 @@ def add_task():
     task_desc = request.json['task_desc']
     add_task_to_db(task_name, task_desc)
     return 'Úloha bola pridaná do databázy.'
-
 
 if __name__ == '__main__':
     app.run()
